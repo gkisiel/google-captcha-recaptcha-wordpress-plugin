80362f95c6a81087bbb50864167dceebe015d961
Grzegorz Kisielewicz <grzegorzk81@gmail.com>
2025-07-12 07:20:39 +0200

    Pumping version to 1.82.2

--- google-captcha.php
+++ google-captcha.php
@@ -6,7 +6,7 @@ Description: Protect WordPress website forms from spam entries with Google Captc
 Author: BestWebSoft & Grzegorz Kisielewicz
 Text Domain: google-captcha
 Domain Path: /languages
-Version: 1.82.1
+Version: 1.82.2
 Author URI: https://bestwebsoft.com/
 License: GPLv3 or later
  */
